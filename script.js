db354e51123fab657a908ad2abde3d8178d2349f
keeran.paheerathan <keeran.paheerathan@gmail.com>
2021-06-07 00:03:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let dayInput = document.querySelector('#day')
+let monthInput = document.querySelector('#month')
+let yearInput = document.querySelector('#year')
+
+let buttonCheck = document.querySelector('button')
+let result = document.querySelector('h2')
+let title = document.querySelector('h1')
+
+let body = document.body
+
+buttonCheck.addEventListener('click', ()=>{
+    let day = dayInput.value
+    let month = monthInput.value
+    let year = yearInput.value
+
+    if(moment(`${day}/${month}/${year}`, 'DD/MM/YYYY', true).isValid()){
+        //Change date to colour
+        day = day.split("").reverse().join("")
+        month = month.split("").reverse().join("")
+        year = year.substring(1,3).split("").reverse().join("")
+
+        let colour = "#" + day + month + year
+
+        //Set text color depending on brightness
+        const brightness = Math.round(((parseInt(day) * 299) + (parseInt(month) * 587) + (parseInt(year) * 114)) / 1000)
+        const textColour = (brightness > 125) ? 'black' : 'white';
+
+        title.style.color = textColour
+        result.style.color = textColour
+
+        //Set background-color and result
+        body.style.backgroundColor = colour
+        result.innerText = "You are " + colour
+    }else{
+        result.innerText = "The date is invalid"
+    }
+})
